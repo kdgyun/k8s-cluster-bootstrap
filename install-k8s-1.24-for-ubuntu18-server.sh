6674b6ffd63f1f68d3504b57c99c68d26d8f174d
kdgyun <kdonggyun97@gmail.com>
2023-07-12 13:34:07 +0900

    path 변경

--- install-k8s-1.24-for-ubuntu18-server.sh
+++ install-k8s-1.24-for-ubuntu18-server.sh
@@ -263,9 +263,9 @@ printstyle "Installing Golang ... \n" 'info'
 lineprint
 wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
-echo 'export GOPATH=$HOME/go' >>~/.profile
-source ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >>${HOME_PATH}/.profile
+echo 'export GOPATH=$HOME/go' >>${HOME_PATH}/.profile
+source ${HOME_PATH}/.profile
 mkdir -p $GOPATH
 go version
 sleep 3
